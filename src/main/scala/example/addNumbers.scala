741840f09c68d7b1e3f1e7c0a81d2a3c757b116a
WameedhLFC <100643842+WameedhLFC@users.noreply.github.com>
2023-06-02 14:37:25 -0500

    Wam message 6-2-2023

---
+++ src/main/scala/example/addNumbers.scala
@@ -0,0 +1,10 @@
+package example
+
+object addNumbers {
+    def main (args: Array[String]): Unit = {
+        println(addMe(39,82))
+    }
+    def addMe(x: Int, y: Int): Int = {
+        x+y
+    }
+}
