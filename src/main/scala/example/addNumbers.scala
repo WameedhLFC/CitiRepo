002d0d3950ee86e0f8239a970cd1e36ac79ba653
WameedhLFC <100643842+WameedhLFC@users.noreply.github.com>
2023-06-02 15:09:40 -0500

    Comment added back 1st time

--- src/main/scala/example/addNumbers.scala
+++ src/main/scala/example/addNumbers.scala
@@ -5,6 +5,7 @@ object addNumbers {
         println(addMe(39,82))
     }
     def addMe(x: Int, y: Int): Int = {
+        // This is a test function - comment added back for 1st time
         x+y
     }
 }
